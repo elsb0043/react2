6c5aab41a56fb157724af2436f48275233680264
elsb0043 <157370211+elsb0043@users.noreply.github.com>
2024-06-24 12:39:52 +0200

    react2

--- src/components/recipeCard/RecipeCard.jsx
+++ src/components/recipeCard/RecipeCard.jsx
@@ -1,6 +1,5 @@
 import { Link } from "react-router-dom"
 import styles from "./recipeCard.module.css"
-import { useState } from 'react'
 import { FaHeart, FaRegHeart  } from "react-icons/fa";
 import { useLocalStorage } from "@uidotdev/usehooks";
 
